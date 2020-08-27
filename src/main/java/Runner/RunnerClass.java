313bac6a1b3d16bdb5a5ca5b6d6c60dbbec97a12
nashnand <anand.muthukrishnan@wirecard.com>
2020-08-27 11:35:06 +0530

    v0.1

--- src/main/java/Runner/RunnerClass.java
+++ src/main/java/Runner/RunnerClass.java
@@ -6,6 +6,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Featurefiles/Opengoogle.feature",glue = "stepDefinitions")
+@CucumberOptions(features="Featurefiles/Opengoogle.feature",
+                 glue = "stepDefinitions",
+                 plugin = {"html:report/WebReport","json:report/jsonreport.json","junit:report/xmlreport.xml"}) /*plugin used to generate report*/
 public class RunnerClass {
 }
